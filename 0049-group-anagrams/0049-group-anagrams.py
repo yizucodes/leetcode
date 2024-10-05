e5ddfd5dc4b61c2da7bb91da9977ae44b63feb63
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2024-10-05 11:50:20 -0400

    Time: 89 ms (60.72%), Space: 21.6 MB (27.30%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,40 +1,24 @@
+from collections import defaultdict
+
 class Solution:
-    
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
-        def createDict(word):
-            wordDict = {}
-
-            for char in word:
-                if char not in wordDict:
-                    wordDict[char] = 1
-                else:
-                    wordDict[char] += 1
- 
-            return wordDict
-
-        # if one string
-        if len(strs) == 1:
+        # One string
+        if len(strs) <= 1:
             return [strs]
-
-        # Dictionary to hold groups of anagrams
-        anagram_map = {}
-
-        # Create a dictionary with letter frequencies as value for each string
-        for word in strs:
-            word_dict = createDict(word)
-
-            # Convert dict to tuple of sorted items for comparison
-            word_tuple = tuple(sorted(word_dict.items()))
-
-            # Group anagrams together
-            if word_tuple in anagram_map:
-                anagram_map[word_tuple].append(word)
-            else:
-                anagram_map[word_tuple] = [word]
-            
-        # Convert anagram_map vals into list of lists
-        res = list(anagram_map.values())
-
-        return res
         
+        wordMap = defaultdict(list)
+
+        for word in strs:
+            # Init 26 zeros list for letters
+            charList = [0] * 26
+            # Find corresponding index of letter of each word in strs
+            for char in word:
+                index = ord(char) - ord('a')
+                charList[index] += 1
+            charListKey = tuple(charList)
+            wordMap[charListKey].append(word)
+
+        result = list(wordMap.values())
+        return result
+
+    
