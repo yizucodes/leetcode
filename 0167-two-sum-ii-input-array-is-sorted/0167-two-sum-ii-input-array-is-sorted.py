8cead296289c24a5a47dd5bf7148c85a9986d799
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-04-14 10:14:26 +0200

    Time: 3 ms (81.37%), Space: 18.7 MB (10.2%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,29 +1,18 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
 
-        # Increase left if sum is smaller, decrease right if sum is larger
-
-        left = 0
-        right = len(numbers) - 1
-
-        res = []
-
-        while left < right:
-            if (numbers[left] + numbers[right]) > target:
-                right -= 1
-            elif numbers[left] + numbers[right] == target:
-                res.append(left + 1)
-                res.append(right + 1)
-                return res
-            else:
-                left += 1
+        l = 0
+        r = len(numbers) - 1
         
-        return res
+        while l < r:
+            currSum = numbers[l] + numbers[r]
+            if currSum == target:
+                return [l + 1, r + 1]
             
-
-
-
-
-
-
+            if currSum > target:
+                r -= 1
+            else:
+                l += 1
+    
+        return []
         
