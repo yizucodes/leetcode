590f020179ee4ce5e6c76503b79143c457615f73
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-04-20 09:26:36 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,36 +1,10 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
-        res = []
+        # length 3 and all 0
+        if len(nums) == 3 and sum(nums) == 0:
+            return [[0,0,0]]
+        if len(nums) == 3 and sum(nums) != 0:
+            return []
+        # what is there are only zeros on multiples of 3?
 
-        numsSorted = sorted(nums)
-      
-
-        for i in range(len(numsSorted)):
-            if i > 0 and numsSorted[i] == numsSorted[i - 1]:
-                continue
-            left = i + 1
-
-            right = len(numsSorted) - 1
-            while left < right:
-                tempSum = numsSorted[i] + numsSorted[left] + numsSorted[right]
-                if tempSum == 0:
-                    res.append([numsSorted[i], numsSorted[left], numsSorted[right]])
-
-                    # Skip duplicates for left and right
-                    while left < right and numsSorted[left] == numsSorted[left + 1]:
-                        left += 1
-                    while left < right and numsSorted[right] == numsSorted[right - 1]:
-                        right -= 1
-                    
-                    left += 1
-                    right -= 1
-
-                elif tempSum < 0:
-                    left += 1
-                elif tempSum > 0:
-                    right -= 1
-
-
-        return res
-
+        # fix one number and then you have a problem left
