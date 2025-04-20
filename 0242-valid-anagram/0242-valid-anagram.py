e1b698c74835c0fd8de003ecf3168553d882c263
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-04-20 08:15:44 -0700

    Time: 6 ms (91.36%), Space: 17.8 MB (86.6%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,21 +1,5 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # create a set and check if there are the same keys and values
-
-        setS = {}
-
-        for char in s:
-            if char not in setS:
-                setS[char] = 1  # Initialize to 1 because it's the first occurrence
-            else:
-                setS[char] += 1  # Increment by 1 for subsequent occurrences
+        return Counter(s) == Counter(t)
         
-        setT = {}
-
-        for char in t:
-            if char not in setT:
-                setT[char] = 1  # Initialize to 1 for the first occurrence
-            else:
-                setT[char] += 1  # Increment by 1 for subsequent occurrences
-
-        return setT == setS  # Compare the two dictionaries
