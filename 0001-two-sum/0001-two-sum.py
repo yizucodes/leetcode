5ff86d885267b3ade99d3347478c30cd7dbfddbb
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-05-14 18:52:09 -0400

    Time: 0 ms (100%), Space: 18.9 MB (34.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,23 +1,28 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # 2 numbers
+        # if there are two numbers in array --> check if the 2 nums add to target
         if len(nums) == 2 and nums[0] + nums[1] == target:
             return [0, 1]
 
-        res = []
+        # map to check
         numMap = {}
 
-        # number: index
+        # iterate through array
         for i in range(len(nums)):
-            currNum = nums[i]
-            complement = target - currNum
-            # if number does not add to target
-            if complement not in numMap:
-                numMap[currNum] = i
-            else:
+            # complement --> target - nums[i]
+            complement = target - nums[i]
+
+            # check complement is in the map
+            if complement in numMap:
+                # if yes, return [indComplement, indCurrNum --> inded of nums[i] ]
                 return [i, numMap[complement]]
+            else:
+                numMap[nums[i]] = i
 
-
-        
-        # no solution
         return []
+        
+
+
+
+
+
