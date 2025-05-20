f56b6f0acae0387f837114b82aaa0dabd1d390ba
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-05-19 23:39:45 -0400

    Time: 91 ms (9.39%), Space: 18.3 MB (81.86%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -1,33 +1,22 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-
-        if k == 0 and sum(nums) == 0:
-            return 0
-
-        # left pointer
-        l = 0
-
-        # maxLen
-        maxLen = 0
-
+        # initialize vars
         numOnes = 0
+        l = 0
+        maxLen = 0
 
         # expand window
         for r in range(len(nums)):
-            numOnes += (nums[r] == 1)
+            if nums[r] == 1:
+                numOnes += 1
 
             # check if window is valid
-            # maxCount = max(maxCount, freqMap[nums[r]])
-            # numOnes = max(numOnes, nums[r])
-
-            # num substitutions in window is greater than allowed k
             while (r - l + 1) - numOnes > k:
                 if nums[l] == 1:
                     numOnes -= 1
                 l += 1
-
+                    
             # update maxLen if window is valid
             maxLen = max(maxLen, r - l + 1)
 
         return maxLen
-
