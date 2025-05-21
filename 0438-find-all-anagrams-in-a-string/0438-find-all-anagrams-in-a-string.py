505afa7bfe9eb1d9b455fb771a41aeb0d364c09a
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-05-21 19:50:17 -0400

    Time: 195 ms (38.62%), Space: 18.2 MB (80.29%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -19,17 +19,11 @@ class Solution:
             # update window map
             freqMapWindow[s[r]] += 1
             
-            while (r - l + 1) > len(p):
+            if (r - l + 1) > len(p):
             # shrink left by 1
             # update window map
                 freqMapWindow[s[l]] -= 1
-                # if freqMapWindow[s[l]] == 0:
-                #     del freqMapWindow[s[l]]
                 l += 1
-
-            # if len window and len p are same
-                # check if windowMap == freqMapP
-                # append left index to res
 
             if (r - l + 1) == len(p):
                 if freqMapWindow == freqMapP:
