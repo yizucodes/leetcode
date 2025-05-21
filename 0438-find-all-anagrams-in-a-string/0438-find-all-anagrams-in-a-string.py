8073de70a83e79a43eac75b23862b50d445fa1eb
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-05-21 19:42:09 -0400

    Time: 208 ms (35.33%), Space: 18.5 MB (36.51%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,21 +1,38 @@
 from collections import Counter
-
-
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        # if len p is greater than len s
         if len(p) > len(s):
             return []
 
         res = []
-        freqMapWindow = Counter(p)
 
-        # p is window
-        for i in range(len(s) - len(p) + 1):
-            endWindow = len(p)
-            chunk = s[i : i + endWindow]
-            freqMapChunk = Counter(chunk)
-            if freqMapWindow == freqMapChunk:
-                res.append(i)
+        # init freqmap for window and p (char : freq)
+        freqMapP = Counter(p)
+        freqMapWindow = Counter()
+
+        l = 0
+        
+        # sliding window
+        for r in range(len(s)):
+            
+            # append char p[r] to window
+            # update window map
+            freqMapWindow[s[r]] += 1
+            
+            while (r - l + 1) > len(p):
+            # shrink left by 1
+            # update window map
+                freqMapWindow[s[l]] -= 1
+                # if freqMapWindow[s[l]] == 0:
+                #     del freqMapWindow[s[l]]
+                l += 1
+
+            # if len window and len p are same
+                # check if windowMap == freqMapP
+                # append left index to res
+
+            if (r - l + 1) == len(p):
+                if freqMapWindow == freqMapP:
+                    res.append(l)
 
         return res
