3d4daf0af4d4e4dd805747764f4d12143c1e7467
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-05-21 20:39:55 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,32 +1,33 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-
-        # empty string --> 0
+        # check for empty
         if len(s) == 0:
             return 0
 
-        # one letter -> 1
+        # check for length 1
         if len(s) == 1:
             return 1
+
+        # init set in currWindow
+        l = 0
+        currMax = 0
+        winSet = set()
+
+        # sliding window
+        for r in range(len(s)):
+
+            # shrink if there is a duplicate
+            while s[r] in winSet:
+                winSet.remove(s[l])
+                l += 1
+            # check if current lenght (r - l + 1) > currMax
+                if (r - l + 1) > currMax:
+                    currMax = r - l + 1
+            
+            winSet.add(s[r])
         
-        maxLen = 0
-        currLen = 0
-        left = 0
+        return currMax
 
-        currChars = set()
-        for right in range(len(s)):
-
-            # if dups, shrink until there is no dups
-            while s[right] in currChars:
-                currChars.remove(s[left])
-                left += 1
-                    
-            currLen = right - left + 1
-            if currLen > maxLen:
-                maxLen = currLen
-            currChars.add(s[right])
-
-        return maxLen
 
 
 
