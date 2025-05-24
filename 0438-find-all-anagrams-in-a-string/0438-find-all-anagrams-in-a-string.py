1226d5cb4b35717b1a67392f8aa8266143313736
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-05-23 20:43:33 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,4 +1,5 @@
 from collections import Counter
+
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         if len(p) > len(s):
@@ -6,27 +7,32 @@ class Solution:
 
         res = []
 
-        # init freqmap for window and p (char : freq)
+        # freqMapP
         freqMapP = Counter(p)
-        freqMapWindow = Counter()
+
+        # winMap
+        winMap = Counter()
 
         l = 0
-        
+
         # sliding window
+            # check if winMap = freqMapP
+                # append the starting index giving by left
+
         for r in range(len(s)):
-            
-            # append char p[r] to window
-            # update window map
-            freqMapWindow[s[r]] += 1
-            
-            if (r - l + 1) > len(p):
-            # shrink left by 1
-            # update window map
-                freqMapWindow[s[l]] -= 1
-                l += 1
+            winMap[s[r]] += 1
 
-            if (r - l + 1) == len(p):
-                if freqMapWindow == freqMapP:
+            if winMap == freqMapP:
+                res.append(l)
+            
+            # shrink windowLen > len(freqMapP)
+            # check for equality
+            # move left += 1
+            while (r - l + 1) > len(p):
+                winMap[s[l]] -= 1
+                if winMap == freqMapP:
                     res.append(l)
-
+                l += 1
+            
         return res
+        
