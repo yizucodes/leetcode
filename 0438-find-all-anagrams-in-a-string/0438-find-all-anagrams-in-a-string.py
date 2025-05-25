44773a9758dfc1d89a3b15f68821c2b431c2ff8e
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-05-25 17:22:21 -0400

    Time: 425 ms (15.62%), Space: 18.5 MB (16.25%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -2,37 +2,33 @@ from collections import Counter
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        # p is longer than s then return []
         if len(p) > len(s):
             return []
 
+        # if same strings then return [0]
+        if s == p:
+            return [0]
+
         res = []
-
-        # freqMapP
-        freqMapP = Counter(p)
-
-        # winMap
-        winMap = Counter()
-
+        freqWindow = Counter()
+        freqP = Counter(p)
         l = 0
 
-        # sliding window
-            # check if winMap = freqMapP
-                # append the starting index giving by left
-
+        # expand if the frequency of char in window is not the same as frequency of char in p
         for r in range(len(s)):
-            winMap[s[r]] += 1
+            char = s[r]
+            freqWindow[char] += 1
 
-            if winMap == freqMapP:
-                res.append(l)
-            
-            # shrink windowLen > len(freqMapP)
-            # check for equality
-            # move left += 1
+        # shrink when window length is bigger than len(p)
             while (r - l + 1) > len(p):
-                winMap[s[l]] -= 1
-                if winMap == freqMapP:
-                    res.append(l)
+                removedChar = s[l]
+                freqWindow[removedChar] -= 1
                 l += 1
             
+            # if length of window is the same
+            # check if freqWindow == freqP
+            if (r - l + 1) == len(p) and Counter(freqWindow) == Counter(freqP):
+                res.append(l)
+
         return res
-        
