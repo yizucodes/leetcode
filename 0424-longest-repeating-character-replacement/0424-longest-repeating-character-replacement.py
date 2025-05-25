497796c04fb11db860406ad8987778dba73d5d73
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-05-25 17:57:21 -0400

    Time: 119 ms (64.23%), Space: 18.1 MB (49.31%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,34 +1,31 @@
+from collections import Counter
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        
 
-        #Initialize left pointer (l), maxCount = 0, empty freq map
+
+        # if k is 0 return maxCharCount
+
+        # if s is one letter, k is 0 then return 1
+
+        maxCharCount = -1
+        freqMapS = Counter(s)
+        freqMapWin = Counter()
         l = 0
-        maxCount = 0
-        freqMap = defaultdict(int)
+        maxLen = -1
 
-#Initialize result (maxLen) = 0
-        maxLen = 0
-
-#Loop through s with right pointer (r):
-   # - Increment count of s[r] in freq map
-    # - Update maxCount if needed
-
+        # expand window when window size - maxCharCount <= ke
         for r in range(len(s)):
-            freqMap[s[r]] += 1
-            maxCount = max(maxCount, freqMap[s[r]])
-    
-    # - If (window size - maxCount) > k:
-            windowSize = r - l + 1
-            if (windowSize - maxCount) > k:
-    # - Shrink window from the left (decrement s[l] in freq map, move l forward)
-                freqMap[s[l]] -= 1
+            char = s[r]
+            freqMapWin[char] += 1
+            maxCharCount = max(maxCharCount, freqMapWin[char])
+            
+        # shrink window window size - maxCharCount > k
+            while (r - l + 1) - maxCharCount > k:
+                removedChar = s[l]
+                freqMapWin[removedChar] -= 1
                 l += 1
-        
-            maxLen = max(maxLen, r - l + 1)
 
-    # - Update maxLen as max(maxLen, current window size)
+        return (r - l + 1)
 
-    # Return maxLen
 
-        return maxLen
-        
