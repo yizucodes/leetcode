f921fdcee17037ebe6230f4c838db7ea5ae933bf
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-05-26 19:43:56 -0400

    Time: 106 ms (22.52%), Space: 18.1 MB (18.75%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,31 +1,29 @@
-from collections import Counter, defaultdict
+from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # init map for s1
-        s1Map = Counter(s1)
+        # if same string return True
+        if s1 == s2:
+            return True
+
+        # initialize vars
         l = 0
-        winFreqMap = Counter()
+        freqS1 = Counter(s1)
+        freqWin = Counter()
 
-        # move right
+        # expand
         for r in range(len(s2)):
-
-            # add s2[right] to windowFreqMap
-            winFreqMap[s2[r]] += 1
+            char = s2[r]
+            freqWin[char] += 1
             
-            # shrink len(window) > len(s1)
+            # shrink when freqS1 is not same as freqWindow
             while (r - l + 1) > len(s1):
-                # remove s[l] from winFreq
-                winFreqMap[s2[l]] -= 1
+                removedChar = s2[l]
+                freqWin[removedChar] -= 1
+                if freqWin == freqS1:
+                    return True
                 l += 1
 
-            if len(s1) == (r - l + 1):
-                if winFreqMap == s1Map:
-                    return True
+            if freqWin == freqS1:
+                return True
 
         return False
-                
-
-
-
-
-            
