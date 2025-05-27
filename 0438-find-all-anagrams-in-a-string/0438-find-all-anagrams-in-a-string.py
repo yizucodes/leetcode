a13df9d380739dbd1140f1b7aa2042fe8e1fed94
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-05-26 20:18:33 -0400

    Time: 303 ms (16.07%), Space: 18.4 MB (36.26%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,34 +1,34 @@
 from collections import Counter
-
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        # p is longer than s then return []
+        # p longer than s > return []
         if len(p) > len(s):
             return []
 
-        # if same strings then return [0]
+        # if same string return [0]
         if s == p:
             return [0]
 
-        res = []
-        freqWindow = Counter()
         freqP = Counter(p)
+        freqWin = Counter()
+        res = []
         l = 0
 
-        # expand if the frequency of char in window is not the same as frequency of char in p
         for r in range(len(s)):
             char = s[r]
-            freqWindow[char] += 1
+            freqWin[char] += 1
 
-        # shrink when window length is bigger than len(p)
-            while (r - l + 1) > len(p):
-                removedChar = s[l]
-                freqWindow[removedChar] -= 1
-                l += 1
-            
-            # if length of window is the same
-            # check if freqWindow == freqP
-            if (r - l + 1) == len(p) and Counter(freqWindow) == Counter(freqP):
+            if freqWin == freqP:
                 res.append(l)
 
+            # shrink when window is greater
+            while (r - l + 1) > len(p):
+                removedChar = s[l]
+                freqWin[removedChar] -= 1
+                l += 1
+                if freqWin == freqP:
+                    res.append(l)
+              
+
         return res
+
