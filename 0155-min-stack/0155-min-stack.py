05bf15a83f4d145ec4528d5b6f86b85183f34708
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-06-08 22:47:15 -0400

    Time: 4 ms (71.44%), Space: 22.6 MB (6.71%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -2,37 +2,33 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-        # self.node = { val: None, mini: None }
 
     def push(self, val: int) -> None:
-        newMini = None
+       
 
-        # if empty list, set minimum to val
-        if len(self.stack) == 0:
-            newMini = val
-        # if not empty, check if val < previousNode min, if it is mini = val else mini remains the same
+        # empty case or
+        # if val < mini then mini = val
+        if len(self.stack) == 0 or val < self.stack[-1]["mini"]:
+            self.stack.append({ "value": val, "mini": val })
         else:
-            prevMini = self.stack[-1]["mini"]
-            newMini = None
-
-            if val < prevMini:
-                newMini = val
-            else:
-                newMini = prevMini
-
-        node = { "value": val, "mini": newMini }
-        self.stack.append(node)
+            self.stack.append({ "value": val, "mini": self.stack[-1]["mini"] })
 
     def pop(self) -> None:
-        self.stack.pop()
-
+        if len(self.stack) > 0:
+            self.stack.pop()
+        
     def top(self) -> int:
-        return self.stack[-1]["value"]
+        if len(self.stack) > 0:
+            return self.stack[-1]["value"]
+        # empty stack
+        else:
+            return []
         
     def getMin(self) -> int:
-        return self.stack[-1]["mini"]
-
+        if len(self.stack) > 0:
+            return self.stack[-1]["mini"]
         
+
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
 # obj.push(val)
