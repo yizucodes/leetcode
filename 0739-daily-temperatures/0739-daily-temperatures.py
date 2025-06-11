fca333ca524e39f2501142ac5f7e386951bcc7ad
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-06-11 14:28:13 -0400

    Time: 93 ms (68.67%), Space: 27.7 MB (45.65%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,13 +1,13 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        res = [0] * len(temperatures)
         stack = []
-        
+        res = [0] * len(temperatures)
+
         for i in range(len(temperatures)):
-            currentIndex = i
-            while stack and temperatures[currentIndex] > temperatures[stack[-1]]:
-                index = stack.pop()
-                res[index] = currentIndex - index
-            stack.append(currentIndex)
+            while stack and temperatures[i] > temperatures[stack[-1]]: # why mono decreasing stack
+                wait_i = stack.pop()
+                res[wait_i] = i - wait_i
+            stack.append(i)
 
         return res
+        
