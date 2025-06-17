40a7dd410f2853c70b7667e90d9adf318ffe5a44
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-06-17 19:14:35 -0400

    Time: 143 ms (56.92%), Space: 32.3 MB (33.42%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -4,27 +4,29 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        # current
-        good = 0
-       
-        def dfs(curr, maxVal):
-            nonlocal good
 
+        # one node return 1
+        good = 0
+
+        def dfs(curr, maxVal):
+            
+            # base case
             if curr is None:
                 return 0
-            
-            # check if curr.val > max --> good + 1
+
+            nonlocal good
+            # check if curr.val >= maxVal --> good += 1
             if curr.val >= maxVal:
                 good += 1
-            
+
+            # adjust maxVal
             newMaxVal = max(curr.val, maxVal)
 
-            left = dfs(curr.left, newMaxVal)
-            right = dfs(curr.right, newMaxVal)
-            
+            dfs(curr.left, newMaxVal)
+            dfs(curr.right, newMaxVal)
+
             return good
 
         return dfs(root, root.val)
