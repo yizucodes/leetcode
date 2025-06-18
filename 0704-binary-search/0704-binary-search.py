412a280644b07bc8f29093f92d5591923961f434
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-06-18 16:22:17 -0400

    Time: 0 ms (100%), Space: 18.8 MB (45.34%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,30 +1,22 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if len(nums) == 1:
-            if target == nums[0]:
-                return 0
-            else:
-                return -1
-            
-        def b_search(nums, target, left, right):
+        # one number case
+        if len(nums) == 1 and nums[0] == target:
+            return 0
 
-            # base case
-            if left > right:
-                return -1
-            
-            m = (left + right) // 2
+        l = 0
+        r = len(nums) - 1
 
-            # when you find the number
+        while l <= r:
+            m = (l + r) // 2
             if nums[m] == target:
                 return m
-            
-            # go right if target >= m
-            if target > nums[m]:
-                return b_search(nums, target, m + 1, right)
+
+            # if num[m] > target --> go left
+            if nums[m] > target:
+                r = m - 1
+            # go right
             else:
-                return b_search(nums, target, left, m - 1)
+                l = m + 1
 
-        
-        return b_search(nums, target, 0, len(nums) - 1)
-
-        
+        return -1
