aac631e0ac9cede7bdee6c55c46594c84547ac20
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-06-22 18:30:41 -0400

    Time: 15 ms (88.88%), Space: 18.3 MB (52.73%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -0,0 +1,45 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        
+        maxA = 0
+        rows, cols = len(grid), len(grid[0])
+
+        def dfs(r, c):
+
+            # base case: out of bounds
+            if r < 0 or r >= rows or c < 0 or c >= cols: 
+                return 0
+            # base case: water or visited
+            if grid[r][c] == 0:
+                return 0
+
+            # mark land as 0 to count as visited
+            grid[r][c] = 0
+            
+            # up
+            aUp = dfs(r - 1, c)
+
+            # down
+            aDown = dfs(r + 1, c)
+
+            # left
+            aLeft = dfs(r, c - 1)
+
+            # right
+            aRight = dfs(r, c + 1)
+
+            return 1 + aUp + aDown + aLeft + aRight
+
+
+
+            
+        # find islands
+        for row in range(rows):
+            for col in range(cols):
+                # do dfs to explore all connected 1s
+                if grid[row][col] == 1:
+                    islandA = dfs(row, col)
+                    maxA = max(maxA, islandA)
+                    
+
+        return maxA
