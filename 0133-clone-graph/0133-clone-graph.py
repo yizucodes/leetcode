07c2302c660f91d98c6183f11986b65af5df7683
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-06-22 12:14:32 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,36 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        # empty graph return
+        if not node:
+            return
+
+        visited = {}
+
+        
+# Better logic:
+# 1. Check if already cloned
+# 2. If not, create clone and store mapping
+# 3. Process neighbors and connect them
+
+
+
+        def dfs(original):
+            cloned = Node(original.val)
+            if original not in visited:
+                # update mapping
+                visited[original] = cloned
+                for nei in original.neighbors:
+                    dfs(nei)
+
+            return cloned
+        
+        return dfs(node)
