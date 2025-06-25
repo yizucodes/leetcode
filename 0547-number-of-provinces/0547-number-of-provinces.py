df6fa858fb4a7cc50e2fd1bfd2ff738b476be242
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-06-25 19:51:56 -0400

    Time: 4 ms (74.1%), Space: 19.3 MB (27.23%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,26 +1,31 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-
+        visited = set()
         numP = 0
-        visited = [0] * len(isConnected)
 
-        # dfs on the other cities
-        # check if other city is not visited
-        # if other_city is 1 means it's connected so do dfs on that other city
-        def dfs(ct):
-            visited[ct] = 1
-            for otherCt in range(len(isConnected[ct])):
-                # visited before
-                if isConnected[ct][otherCt] == 1 and visited[otherCt] == 0:
-                    dfs(otherCt)
-            
-            return ct
-    
-        # traverse cities which is index in isConnected
+        # if not visited start province
+        def dfs(city):
+
+            "Mark the current city as visited so I remember I've been here"
+            visited.add(city)
+
+            "Start looking at every single city that exists in the whole map"
+            for other in range(len(isConnected)):
+                # Is there a direct road between my current city and this other city?
+                if isConnected[city][other] == 1:
+                    "If I found a connection, then check if I've already visited that connected city before"
+                    if other not in visited:
+                        dfs(other)
+                    "If there's a connection AND I haven't been there yet, then go visit that city and repeat this whole process from there"
+
+        # loop through cities
         for city in range(len(isConnected)):
-            # city has not been visited
-            if visited[city] == 0:
+            if city not in visited:
                 numP += 1
                 dfs(city)
 
+       
+
+
+        # count provinces
         return numP
