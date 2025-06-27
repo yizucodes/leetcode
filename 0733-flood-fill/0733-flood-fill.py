6aaa4f5aca41b77acdea36e4443704b1c0541a8f
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-06-27 12:23:27 -0400

    Time: 0 ms (100%), Space: 18.1 MB (51.05%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,36 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+
+        # The starting pixel is already colored with 0, which is the same as the target color. Therefore, no changes are made to the image.
+        startingColor = image[sr][sc]
+        if startingColor == color:
+            return image
+
+        # dfs
+        rows, cols = len(image), len(image[0])
+        
+        def dfs(r, c):
+
+            # boundary checks
+            # if 0 < r or r >= rows:
+            #     return
+            # if 0 < c or c >= cols:
+            #     return 
+
+            # base case: startingColor same as cell color
+            # return
+            if startingColor != image[r][c]:
+                return
+
+            # modify in place to color
+            image[r][c] = color
+
+            directions = [(-1, 0), (1,0), (0, -1), (0, 1)]
+            # dfs up, down, left and right
+            for dr, dc in directions:
+                newR, newC = dr + r, dc + c
+                if 0 <= newR < rows and 0 <= newC < cols:
+                    dfs(newR, newC)
+
+        dfs(sr, sc)
+        return image
