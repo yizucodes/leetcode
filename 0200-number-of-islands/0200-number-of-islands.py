f7a08a4bf145544cf10816016c05dda6be7cf01a
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-07-04 10:30:01 -0400

    Time: 232 ms (87.11%), Space: 20 MB (95.6%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,44 +1,32 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        # no grid
-        if not grid:
-            return 0
-
-        # recursively check if there is adjacent land
         numI = 0
         rows, cols = len(grid), len(grid[0])
 
         def dfs(r, c):
-            
-            # base case: bounds
-
-            if c < 0 or c >= cols:
-                return
-
+            # boundaries we are in the grid
             if r < 0 or r >= rows:
                 return
-
-            # base case: water
+            if c < 0 or c >= cols:
+                return
+            # return if it's a 0
             if grid[r][c] == "0":
                 return
 
-            # mark as visited by marking as 0
+            # update grid with 0
             grid[r][c] = "0"
 
-            # up, down, left, right
-            # call dfs again on new coordinates
-
-            dfs(r - 1, c)
+            # visit top, down, left and right with dfs
             dfs(r + 1, c)
+            dfs(r - 1, c)
             dfs(r, c - 1)
             dfs(r, c + 1)
 
-        # call dfs for each coordinates
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == '1':
+        # find first "1", numI += 1, call dfs
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == "1":
                     numI += 1
-                    dfs(r, c)
-        
+                    dfs(row, col)
+
         return numI
