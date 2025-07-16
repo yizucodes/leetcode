42387115589b141f4b86130d6108ee76834f545b
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-07-16 11:05:34 -0400

    Time: 2 ms (85.58%), Space: 18.5 MB (59.43%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,19 +1,17 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # check for empty --> empty array
-        left = 0
-        n = len(numbers)
-        right = n - 1
-        
-        while left < right:
-            currSum = numbers[left] + numbers[right]
-            if currSum == target:
-                # ensure to add 1 to both indices as it's one indexed
-                return [left + 1, right + 1]
-            elif currSum > target:
-                right -= 1
-            # currSum < target
-            else:
-                left += 1
+        l = 0
+        r = len(numbers) - 1
 
-        return res
+        while l < r:
+            currSum = numbers[l] + numbers[r]
+
+            if currSum == target:
+                return [l + 1, r + 1]
+
+            if currSum > target:
+                r -= 1
+            else:
+                l += 1
+        
+        return []
