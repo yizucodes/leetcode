792bd4d923406c7b2bd3f24c8a75d77d8f4774e6
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-07-17 13:25:02 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,33 +1,20 @@
-from collections import Counter
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        # p longer than s > return []
-        if len(p) > len(s):
-            return []
+        # if p is empty --> return []
 
-        # if same string return [0]
-        if s == p:
-            return [0]
+        # p is shorter than s 
 
-        freqP = Counter(p)
-        freqWin = Counter()
-        res = []
         l = 0
-
-        for r in range(len(s)):
-            char = s[r]
-            freqWin[char] += 1
-
-            if freqWin == freqP:
+        r = len(p) - 1
+        res = []
+        while r < len(s):
+            window = s[l : r + 1] # array
+            
+            if Counter(window) == Counter(p):
                 res.append(l)
+            
+            l += 1
+            r += 1
 
-            # shrink when window is greater
-            while (r - l + 1) > len(p):
-                removedChar = s[l]i
-                freqWin[removedChar] -= 1
-                l += 1
-                if freqWin == freqP:
-                    res.append(l)
-              
+        
         return res
-
