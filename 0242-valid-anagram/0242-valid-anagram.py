d84ab5dab2eb9fe53754c90d7f6d6b9f1ed26b80
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-07-17 12:10:03 -0400

    Time: 7 ms (89.16%), Space: 18 MB (33.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,24 +1,5 @@
 from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # create a map with letter and frequency for s 
-        freqMap = Counter(s)
-        
-        # iterate through this map for t
-        for char in t:
-
-        # if letter is not in there or letter frequency is 0
-        # return False
-            if char not in freqMap or freqMap[char] == 0:
-                return False
-        
-        # decrease frequency as letter exists
-            freqMap[char] -= 1
-
-        # Return as it means that all the letters have freq 0
-        # If there is any letter with a freq > 0 return False
-        for _, freq in freqMap.items():
-            if freq > 0:
-                return False
-        return True
+        return Counter(s) == Counter(t)
         
