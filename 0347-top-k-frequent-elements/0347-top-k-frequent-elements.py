8ce991641a2e6d292b52487a6a3561792f1d5310
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-07-21 20:25:06 -0400

    Time: 7 ms (51.63%), Space: 21.4 MB (29.71%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,22 +1,17 @@
+from collections import Counter
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # one item return nums
-        if len(nums) == 1:
-            return nums
+        # Step 1: Count frequencies
+        # {1: 3, 2: 2, 3: 1}
 
-        # create dictionary for each unique number in nums
-        numDict = {}
-        for nume in nums:
-            if nume not in numDict:
-                numDict[nume] = 1
-            else:
-                numDict[nume] += 1
+        numMap = Counter(nums)
 
-        # Sort dictionary items by value (freq) from high to low
-        sorted_items = sorted(numDict.items(), key = lambda x: x[1], reverse=True)
+        # Step 2: Sort by frequency (somehow)
+        # [(1, 3), (2, 2), (3, 1)]  # (element, frequency) pairs
+        mostCommon = numMap.most_common(k)
 
-        # Return k most requent elements
-        return [item[0] for item in sorted_items[:k]]
-
-        
+        # Step 3: Take top k elements
+        # Return [1, 2]
+        res = [element for element, freq in mostCommon]
+        return res
