ae4ef439b8d75bb040102b8eb30351e3e918b330
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-10-28 11:39:54 -0700

    Time: 1 ms (87.79%), Space: 57.1 MB (33.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,23 +4,20 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    if (nums.length == 2 && nums[0] + nums[1] === target) {
-        return [0, 1]
-    }
+    const numMap = {}
 
-    const numsMap = {}
+    for (let i = 0; i < nums.length; i++) {
+        const comp = target - nums[i]
     
-    for(let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i]
-        if (complement in numsMap) {
-            return [i, numsMap[complement]]
+        // number exists
+        if (nums[i] in numMap) {
+            return [i, numMap[nums[i]]]
+        } 
+        // add to map comp : i
+        else {
+            numMap[comp] = i
         }
-
-        numsMap[nums[i]] = i
     }
-
-    // create a numMap with the num: index
-    // check if the number in array is complement of any number in map
-
     return []
+    
 };
