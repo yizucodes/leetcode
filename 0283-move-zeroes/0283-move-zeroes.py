b3774c50181e182220626b2129fbb8ff25e09691
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-11-26 11:04:43 -0800

    Time: 0 ms (100%), Space: 18.9 MB (31.4%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,15 +3,35 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
+
+        if len(nums) == 1:
+            return nums
+
         write = 0
-        for read in range(len(nums)):
-            if nums[read] != 0:
-                nums[write] = nums[read]
-                write += 1
         
-        while write <= read:
+        # read pointer reads non-zero
+        
+
+        # if read pter reads non-zero element, write pointer = read value
+        # write += 1
+
+        # once read pter traverses entire array, write marks 0 the remaining values
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[write] = nums[i]
+                write += 1
+
+        while write < len(nums):
             nums[write] = 0
             write += 1
-        
+
         return nums
+
+
+        
+
+
+
+   
+        
