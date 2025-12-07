988a18477227ab9079a44e899ee60357f26e7fe9
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2025-12-07 13:27:57 -0800

    Time: 20 ms (25.35%), Space: 20.6 MB (88.65%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,12 +2,24 @@ from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # if len is 1
+        # return [strs]
+        if len(strs) == 1:
+            return [strs]
         
-        canMap = defaultdict(list)
-        
+        keyMap = defaultdict(list)
+        # unique sorted keys for strs array
         for word in strs:
-            # sort word alphabetically
-            canonical = ''.join(sorted(word)) # "eat" -> "aet"
-            canMap[canonical].append(word)
+            sortedKey = "".join(sorted(word))
+            if sortedKey not in keyMap:
+                keyMap[sortedKey] = []
+        
+        # add words based on the sortedKey
+        for word in strs:
+            sortedKey = "".join(sorted(word))
+            if sortedKey in keyMap:
+                keyMap[sortedKey].append(word)
 
-        return list(canMap.values())
+        # return the values in an array by iterating through the map
+        return [val for val in keyMap.values()]
+
