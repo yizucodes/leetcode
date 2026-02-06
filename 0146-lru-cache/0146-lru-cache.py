6760416d10739fb2818c880765db97c3caa9c63a
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2026-02-06 12:04:12 -0800

    Time: 96 ms (91%), Space: 78.8 MB (14.36%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,72 +1,79 @@
 class Node:
-    def __init__(self, key=None, val=None):
+    def __init__(self, key = None, value = None):
         self.key = key
-        self.val = val
+        self.value = value
         self.prev = None
         self.next = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        if capacity < 1:
-            print("Capacity must be at least 1")
-        self.capacity = capacity
         self.cache = {}
+        self.capacity = capacity
         self.head = Node()
-        self.tail = Node()
+        self.tail = Node()   
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def _remove(self, node: Node):
-        # remove node from the list
-        tempPrev = node.prev
-        tempNext = node.next
-
-        node.prev.next = tempNext
-        node.next.prev = tempPrev
+    def _remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
     
     def _add_to_front(self, node):
-        # insert node into the list
-        
-        # Save next item
-        first = self.head.next
-        self.head.next = node
         node.prev = self.head
-        node.next = first
-        first.prev = node
+        node.next = self.head.next
+        self.head.next = node
+        node.next.prev = node
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
+        # access node cache[key]
         node = self.cache[key]
+
+        # remove
         self._remove(node)
+
+        # add node to the front
         self._add_to_front(node)
 
-        return node.val
+        # return the value of the node
+        return node.value
 
     def put(self, key: int, value: int) -> None:
-        # IF key exists: get node, update value, move to front
+        # key in cache
         if key in self.cache:
+
+            # update the key and value
             node = self.cache[key]
-            node.val = value
+
+            # update node value
+            node.value = value
+
+            # remove node
             self._remove(node)
+
+            # add to the front of the list
             self._add_to_front(node)
-        # ELSE: create new node, add to cache dict, add to front, check capacity
+
+        # else: key not cache
         else:
-            newNode = Node(key, value)
-            self.cache[key] = newNode
-            self._add_to_front(newNode)
+            # create new node
+            node = Node(key, value)
+            # add to cache
+            self.cache[key] = node
+            # add to front
+            self._add_to_front(node)
+
+            # if capacity exceeded
+            if len(self.cache) > self.capacity:
+                lru_node = self.tail.prev
+                # remove the lru key which is the last node before tail
+                self._remove(lru_node)
+                # remove from cache
+                del self.cache[lru_node.key]
+    
         
-        if len(self.cache) > self.capacity:
-            lru_node = self.tail.prev
-            self._remove(lru_node)
-            del self.cache[lru_node.key]
-            
-
-
-
-        
-
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
