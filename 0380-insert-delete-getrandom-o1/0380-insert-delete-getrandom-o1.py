2edd324f20b7db5da018fa50197c261c249e9198
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2026-02-09 12:24:19 -0800

    Time: 93 ms (86.74%), Space: 56.9 MB (62.56%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -3,47 +3,38 @@ import random
 class RandomizedSet:
 
     def __init__(self):
-        self.data = []
-        # value : index (index from self.data)
-        self.val_to_idx = {}
-        
+        self.list = []
+        self.valToIndex = {}
+
     def insert(self, val: int) -> bool:
-        if val in self.val_to_idx:
+        if val in self.valToIndex:
             return False
-
-        self.data.append(val)
-        self.val_to_idx[val] = len(self.data) - 1
+        self.list.append(val)
+        self.valToIndex[val] = len(self.list) - 1
         return True
-        
+
     def remove(self, val: int) -> bool:
-        # check from dictionary if value exist
-        if val not in self.val_to_idx:
+        if val not in self.valToIndex:
             return False
 
-        removeIndex = self.val_to_idx[val]
-
-        # last element of list
-        lastElement = self.data[-1]
-
-        # swap
-        self.data[removeIndex], self.data[-1] = self.data[-1], self.data[removeIndex]
-
-        # pop last element
-        self.data.pop()
-
-        # update dictionary with new index of swapped  --> the swappedItemValue has removeIndex as value
-        self.val_to_idx[lastElement] = removeIndex
-
-        # remove deleted value from dictionary
-        del self.val_to_idx[val]
-
+        lastEl = self.list[-1]
+        
+        indexVal = self.valToIndex[val]
+        self.valToIndex[lastEl] = indexVal
+        del self.valToIndex[val]
+        
+        # remove value
+        # swap index of last element with val
+        self.list[indexVal], self.list[-1] = self.list[-1], self.list[indexVal]
+        self.list.pop()
         return True
 
     def getRandom(self) -> int:
-        # random index
-        randomIndex = random.randint(0, len(self.data) - 1)
-        return self.data[randomIndex]
-
+        # random number from 0 to len(list) - 1
+        if not self.list:
+            return
+        randomIndex = random.randint(0, len(self.list) - 1)
+        return self.list[randomIndex]
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
