bfff74fd1dfff99d5277549c5ac7cc6060bb823a
Yi Zu <39059002+yizucodes@users.noreply.github.com>
2026-02-12 11:48:38 -0800

    Time: 4 ms (70.76%), Space: 23.2 MB (24.22%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,16 +1,21 @@
-from collections import Counter
+from collections import Counter 
+import heapq
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # num: freq with Counter
-        numFreq = Counter(nums)
+        # freqMap
+        freqMap = Counter(nums)
+        minHeap = []
 
-        # list comprehension to return the keys of the most common k values
-        return [num for num, _ in numFreq.most_common(k)]
+        # create minHeap with tuples (freq, element)
+        for el, freq in freqMap.items():
+            if len(minHeap) < k:
+                # push top of heap
+                heapq.heappush(minHeap, (freq, el))
+            # elif len(heap) == k and freq of element > freq top element of heap: 
+            elif len(minHeap) == k and freq > minHeap[0][0]:
+                # push new to min heap only if the freq is higher than the top value
+                heapq.heappop(minHeap)
+                heapq.heappush(minHeap, (freq, el))
 
-# Time complexity: O(n + m log k)
-# Space complexity: O(m + k)
-
-# n = length of array
-# m = distinct values in nums
-# k = input param
+        return [tup[1] for tup in minHeap]
